cef26c6c2fece8c2a97eafdc3d2dcc01236b3bda
austincunningham <austincunningham@hotmail.com>
2016-06-04 00:36:02 +0100

    moved javascript to file donate.js, added form error checking to js

---
+++ public/javascripts/donate.js
@@ -0,0 +1,21 @@
+/** force semantic ui forms to work*/ 
+$('.ui.dropdown').dropdown();
+       
+
+$('.ui.dropdown')
+  .dropdown()
+;
+
+$('.ui.form')
+.form({
+  amountDonated: {
+    identifier: 'amountDonated',
+    rules: [
+      {
+        type: 'empty',
+    prompt: 'You forgot to give us money , Please select amount to donate'
+      }
+    ]
+  }
+})
+;
